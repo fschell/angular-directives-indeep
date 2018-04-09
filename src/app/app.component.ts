2a05403c89c6f8e9cbf6f5fdc4c247d73c649ece
fs <fs@imagine-orange.com>
2018-04-09 14:46:05 +0200

    content binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import {Component, Directive, HostBinding} from '@angular/core';
-
+import {Component, Directive, ElementRef, ViewContainerRef, TemplateRef, Input} from '@angular/core';
 
 @Directive({
-    selector: 'h1'
+    selector: '[three]'
 })
-export class FirstDirective {
-  @HostBinding() innerText = 'I am a directive';
+export class ThreeDirective {
+    @Input() set three(value) {
+        this.view.createEmbeddedView(this.template, {
+            $implicit: 'Awesome'
+        });
+        this.view.createEmbeddedView(this.template, {
+            $implicit: 'Amazing'
+        });
+        this.view.createEmbeddedView(this.template, {
+            $implicit: 'Sweet'
+        });
+    }
 
+    constructor(el: ElementRef,
+                private view: ViewContainerRef,
+                private template: TemplateRef<any>) {
+        console.log(el.nativeElement);
+    }
 }
 
 @Component({
-  selector: 'app-root',
-  template: '<h1>myApp</h1>',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `
+        <h1 *three="let message">{{message}}</h1>
+    `
 })
 export class AppComponent {
-  title = 'app';
 }
