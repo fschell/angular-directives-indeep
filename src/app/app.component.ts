a3fed50a8278c2f69adc846e1d842a8810aaf2ad
fs <fs@imagine-orange.com>
2018-04-09 14:34:10 +0200

    dynamic content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,51 @@
-import {Component, Directive, HostBinding} from '@angular/core';
-
+import {Component, Directive, ElementRef, ViewContainerRef, TemplateRef, Input} from '@angular/core';
 
 @Directive({
-    selector: 'h1'
+    selector: '[three]'
 })
-export class FirstDirective {
-  @HostBinding() innerText = 'I am a directive';
+export class ThreeDirective {
+    @Input() set threeFrom({one, two, three}) {
+        this.view.clear();
 
+        this.view.createEmbeddedView(this.template, {
+            $implicit: one
+        });
+        this.view.createEmbeddedView(this.template, {
+            $implicit: two
+        });
+        this.view.createEmbeddedView(this.template, {
+            $implicit: three
+        });
+    }
+
+    constructor(el: ElementRef,
+                private view: ViewContainerRef,
+                private template: TemplateRef<any>) {
+        console.log(el.nativeElement);
+    }
 }
 
 @Component({
-  selector: 'app-root',
-  template: '<h1>myApp</h1>',
-  styleUrls: ['./app.component.css']
+    selector: 'app',
+    template: `
+        <h1 *three="let message from messages">{{message}}</h1>
+    `
 })
 export class AppComponent {
-  title = 'app';
+    messages = {
+        one: 'One is awesome',
+        two: 'Two is better',
+        three: 'Three is best!'
+    };
+
+    constructor() {
+        setInterval(() => {
+            this.messages = {
+                one: 'One ' + Math.random().toString().slice(0, 3),
+                two: 'Two ' + Math.random().toString().slice(0, 3),
+                three: 'Three ' + Math.random().toString().slice(0, 3)
+            };
+        }, 1000);
+    }
 }
+
